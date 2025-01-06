a53ae7e1e3dca1658b0fd3e28e4c77e683d0056a
Fauve <fauve.mareels@outlook.com>
2025-01-06 12:48:09 +0100

    Création d'un Tableau et Exporter vers main.js

---
+++ WEEK-15/part.js
@@ -0,0 +1,7 @@
+// Objectif divisé le code entre deux fichiers (main.js et part.js),
+
+// Déclarer un tableau  
+let Tableau = ["Chaine1", "Chaine2", "Chaine3"];
+
+// Exporter le le fichier pour qu'il puisse être utilisé dans main.js
+Module.exports = Tableau;
