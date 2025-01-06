768a463ed4d6bc337a200117fa93fdc868d1b3d5
Fauve <fauve.mareels@outlook.com>
2025-01-06 13:19:41 +0100

    Affichier les nouvelles valeurs dans main.js

--- WEEK-15/main.js
+++ WEEK-15/main.js
@@ -1,4 +1,9 @@
 // Importer le Tableau de part.js
-const Tableau = require('./part');
+// const Tableau = require('./part');
+
+const {Tableau, pi, Objet } = require('./part');
+
 // L'afficher dans la console.
 console.log(Tableau);
+console.log(pi);
+console.log(Objet);
