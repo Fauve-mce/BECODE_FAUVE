268e0c41a1ff12cf81de1344752e9a0737ad7f16
Fauve <fauve.mareels@outlook.com>
2025-01-17 14:04:21 +0100

    Add posts data and logic to check for empty posts array

--- WEEK-15/14.MoreEJS.md/ServerMoreEJS.js
+++ WEEK-15/14.MoreEJS.md/ServerMoreEJS.js
@@ -14,3 +14,36 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+
+
+const posts = [
+    {
+        title: "Post one",
+        description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tempora! Voluptatibus, quod.",
+        author: "King Philippe",
+    },
+    {
+        title: "Post two",
+        description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tempora! Voluptatibus, quod.",
+        author: "Barack Obama",
+    },
+    {
+        title: "Post three",
+        description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tempora! Voluptatibus, quod.",
+        author: "Nelson Mandela",
+    },
+];
+
+app.get("/", (req, res) => {
+    // Vérifie si le tableau `posts` est vide
+    if (posts.length === 0) {
+      // Si le tableau est vide, renvoie la vue 'index' avec la variable 'posts' définie sur 'null'
+        return res.render("index", { posts: null });
+    }
+    // Si le tableau `posts` n'est pas vide, renvoie la vue 'index' avec la variable 'posts' contenant les données des posts
+    res.render("index", { posts: posts });
+});
